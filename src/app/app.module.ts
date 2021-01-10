6aebb1f6f9aa5142e6d713feb7da552760156247
briank76 <briank76@gmail.com>
2021-01-10 12:31:21 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './loader/loader.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+import { DynamicComponentBase } from './dynamicComponentBase/dynamic-base-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent,
+    Test1Component,
+    Test2Component,
+    DynamicComponentBase
   ],
   imports: [
     BrowserModule,
